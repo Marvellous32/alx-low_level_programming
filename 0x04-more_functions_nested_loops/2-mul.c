5af3e3fc33588323bdf19fd34267fd89e29dba1c
marvellouschimuanya <chimarvellous32@gmail.com>
2022-09-16 15:41:08 -0700

    this is my correction

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,7 +3,6 @@
 
 
 /**
- *
  *  * mul - multiplier
  *
  *   * @a: int to mult
@@ -11,15 +10,10 @@
  *    * @b: int to mult
  *
  *     * Return: result of two
- *
- *      */
-
-
+ */
 
 int mul(int a, int b)
 
 {
-
-		return (a * b);
-
+	return (a * b);
 }
