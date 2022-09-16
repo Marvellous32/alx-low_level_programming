9577a743f5f2381e47bcde3cf6d1d65201d9c030
marvellouschimuanya <chimarvellous32@gmail.com>
2022-09-16 16:45:55 -0700

    more corrections

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print-diagonal - prints diagonal stuff
+ *
+ * @n: integer to set diagonal
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
