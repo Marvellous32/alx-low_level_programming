170d216a2fbaca54e69266f040f94456ac5ed94a
marvellouschimuanya <chimarvellous32@gmail.com>
2022-10-05 16:36:26 -0700

    my correction

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,13 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _memcpy - copies memory area
  * @dest: destination of copy
  * @src: memory area to copy
  * @n: number of bytes to copy
- *
  * Return: pointer to dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
